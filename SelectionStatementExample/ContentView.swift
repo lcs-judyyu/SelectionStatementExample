e02a115260c1bc0bcc5cba0b48a9987f24e19278
Judy <jyu@lcs.on.ca>
2021-12-02 09:52:32 -0500

    Add a random target and add selective statements to provide feedback

--- SelectionStatementExample/ContentView.swift
+++ SelectionStatementExample/ContentView.swift
@@ -13,6 +13,13 @@ struct ContentView: View {
     //current guess of user
     @State var currentGuess: Double = 61
     
+    //The target we are trying to get
+    //random number from 1 to 100
+    let target = Int.random(in: 1...100)
+    
+    //feedback
+    @State var feedback = "Take a guess!" //No hint to start the game
+    
     var body: some View {
         
         VStack(alignment: .leading, spacing: 20) {
@@ -47,12 +54,43 @@ struct ContentView: View {
                 Spacer()
                 
                 Button(action: {
+                    
+                    //make user's guess (Double) into an Int
+                    let currentGuessAsInteger = Int(currentGuess)
+                    
+                    //Compare current guess to the target
+                    if currentGuessAsInteger < target {
+                        //guess higher
+                        feedback = "Guess higher"
+                    } else if currentGuessAsInteger > target {
+                        //guess lowerer
+                        feedback = "Guess lower"
+                    } else {
+                        feedback = "You got it!"
+                    }
+                    
                     print("Button was pressed")
                 }, label: {
                     Text("Submit Guess")
                 })
                     .buttonStyle(.bordered)
                     .padding(.top, 10)
+                
+                Spacer()
+            }
+            
+            
+            HStack {
+                
+                Spacer()
+                
+                VStack(alignment: .center) {
+                    Text("The super secret target is \(target)")
+                        .padding(.bottom, 20)
+                    
+                    Text(feedback)
+                        .italic()
+                }
                 
                 Spacer()
             }
